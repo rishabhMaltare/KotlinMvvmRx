15f43040b9f7a9b22fb9e1de0402b34f55ff8af8
Rishabh <maltarerishabh@gmail.com>
2018-06-16 16:58:01 +0530

    Added DataBinding support

--- app/src/main/java/com/rishabh/kotlinmvvmrx/MainActivity.kt
+++ app/src/main/java/com/rishabh/kotlinmvvmrx/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.rishabh.kotlinmvvmrx
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.rishabh.kotlinmvvmrx.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
--- app/src/main/java/com/rishabh/kotlinmvvmrx/models/Head.kt
+++ app/src/main/java/com/rishabh/kotlinmvvmrx/models/Head.kt
@@ -8,12 +8,15 @@ class Head {
     @SerializedName("label")
     @Expose
     var label: String? = null
+
     @SerializedName("ref")
     @Expose
     var ref: String? = null
+
     @SerializedName("sha")
     @Expose
     var sha: String? = null
+
     @SerializedName("repo")
     @Expose
     var repo: Repo? = null
